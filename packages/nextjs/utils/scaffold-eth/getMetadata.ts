d00cae6af4df90d5927841c74e96af43254ae440
Austin Griffith <austin@ethereum.org>
2026-03-18 19:35:05 -0600

    fix: remove broken localhost OG image
    Also added to deploy checklist:
    - Run: forge verify-contract <ADDR> TodoList --chain-id 8453 --rpc-url <RPC>

--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -8,40 +8,22 @@ const titleTemplate = "%s";
 export const getMetadata = ({
   title,
   description,
-  imageRelativePath = "/thumbnail.jpg",
 }: {
   title: string;
   description: string;
-  imageRelativePath?: string;
 }): Metadata => {
-  const imageUrl = `${baseUrl}${imageRelativePath}`;
-
   return {
-    metadataBase: new URL(baseUrl),
     title: {
       default: title,
       template: titleTemplate,
     },
     description: description,
-    openGraph: {
-      title: {
-        default: title,
-        template: titleTemplate,
-      },
-      description: description,
-      images: [
-        {
-          url: imageUrl,
-        },
-      ],
-    },
     twitter: {
       title: {
         default: title,
         template: titleTemplate,
       },
       description: description,
-      images: [imageUrl],
     },
     icons: {
       icon: [
